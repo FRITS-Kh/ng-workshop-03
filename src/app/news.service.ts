4184b66602ec29a6d59cfcd390498c0a94fb5ae8
Yuriy Gerton <yuriy.gerton@spryker.com>
2020-02-03 18:54:14 +0200

    hw-3

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,7 +1,7 @@
 // tslint:disable: variable-name
 import { Injectable } from '@angular/core';
-import { combineLatest, OperatorFunction, Subject } from 'rxjs';
-import { map, scan, shareReplay, startWith } from 'rxjs/operators';
+import {merge, OperatorFunction, Subject} from 'rxjs';
+import {map, scan, shareReplay, startWith, withLatestFrom} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +10,24 @@ export class NewsService {
   private _news$ = new Subject<string>();
   private _breakingNews$ = new Subject<string>();
 
-  news$ = this._news$.pipe(storeNews());
-  breakingNews$ = this._breakingNews$.pipe(storeNews());
+  news$ = this._news$.pipe(storeNews(), shareReplay());
+  breakingNews$ = this._breakingNews$.pipe(storeNews(), shareReplay());
 
-  allNews$ = combineLatest(
+  allNews$ = merge(
     // combineLatest will start emitting when all of it's streams will emit at least one time
     // that is why startWith() operator is required here - we want to emit normal news
     // even when there were no breaking news yet and vice versa
-    this.breakingNews$.pipe(startWith([])),
-    this.news$.pipe(startWith([]))
-  ).pipe(map(([breaking, news]) => [...breaking, ...news]));
+    this._breakingNews$,
+    this._news$
+  ).pipe(
+      withLatestFrom(
+        this.news$.pipe(startWith(null)),
+        this.breakingNews$.pipe(startWith(null))
+      ),
+      map(([news]) => news ),
+      storeNews(),
+      shareReplay()
+  );
 
   addNews(news: string) {
     this._news$.next(news);
